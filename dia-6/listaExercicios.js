b795f4e0470529e59a2800a0d3a06d195716f835
Erandir Mota <erandirsm13@gmail.com>
2023-08-22 23:06:49 -0300

    update new exercise

---
+++ dia-6/listaExercicios.js
@@ -0,0 +1,13 @@
+/*
+let value = parseInt(prompt("Type a number: "));
+
+for (let i = 0; i <= value; i++) {
+  console.log(i);
+}
+*/
+
+// Contagem de 0 a 50 pulando de 5 em 5
+for (let i = 0; i <= 50; i++) {
+  let num = i + 5;
+  console.log(num);
+}
---
+++ dia-6/node_6e43df72e837c.tmp.js
@@ -0,0 +1,12 @@
+/*
+let value = parseInt(prompt("Type a number: "));
+
+for (let i = 0; i <= value; i++) {
+  console.log(i);
+}
+*/
+
+// Contagem de 0 a 50 pulando de 5 em 5
+for (let i = 0; i <= 50; i + 5) {
+  console.log(i);
+}
