b202ccad1f041cf3dce742cfd28e32bc0c77b4cb
Erandir Mota <erandirsm13@gmail.com>
2023-08-29 01:19:51 -0300

    making new excersise

---
+++ dia-9/script.js
@@ -0,0 +1,41 @@
+// Getting personal informations and saving
+let name = prompt("Type your name: ");
+let age = parseInt(prompt("Type you age: "));
+let weight = parseInt(prompt("Type your weight: "));
+let height = prompt("Type your height: ");
+let profession = prompt("Type your profession: ");
+
+// Displaying the personal informations
+console.log(`Hello ${name}, you're ${age} years old, is a ${profession}, have ${height}m and weighs ${weight}kg.`);
+
+// Checking if the person can to drink
+if (age >= 18) {
+  console.log("You're released to drink some cold ones!");
+} else {
+  console.log("No some cold ones for you dude.");
+}
+
+// Transferring age in months, weeks and days
+let ageMonths = age * 12;
+let ageWeeks = age * 48;
+let ageDays = age * 365;
+
+// Displaying the informations
+console.log(`Your age in months: ${ageMonths}, weeks: ${ageWeeks}, days: ${ageDays}`);
+
+// Calculating the person's IMC
+let imc = weight / (height * height);
+
+// Checking the IMC result
+console.log("------ IMC ------");
+if (imc < 18.5) {
+  console.log("Thinness");
+} else if (imc >= 18.5 && imc <= 24.9) {
+  console.log("Normal");
+} else if (imc >= 24.9 && imc <= 30) {
+  console.log("Overweight");
+} else {
+  console.log("Obesity");
+}
+
+
