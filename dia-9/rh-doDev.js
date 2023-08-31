bea80d5806cbd2afef23d626d1ffc9f94bfe45f6
Erandir Mota <erandirsm13@gmail.com>
2023-08-30 23:39:50 -0300

    att exercise

---
+++ dia-9/rh-doDev.js
@@ -0,0 +1,20 @@
+ let clientName;
+ let clientAge = 0;
+ let clientWage = 0;
+ 
+ let continuing = true;
+ while (continuing) {
+  clientName = prompt("Type your name: ");
+  clientAge = parseInt(prompt("Type your age: "));
+  clientWage = parseFloat(prompt("Type your wage: "));
+
+  console.log(`Hello ${clientName}, you're ${clientAge} years old, your wage is ${clientWage}.`)
+
+  let wishContinue = parseInt(prompt("The informations are correct? (1) yes or (2) no: "));
+  if (wishContinue === 1) {
+    continuing = false;
+  }
+ }
+ 
+
+
